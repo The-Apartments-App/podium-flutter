373b45a282032e766427e99e505d8030ab65f930
Darren Via <aivnerrad1993@gmail.com>
2022-12-24 11:38:51 -0800

    update

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,12 +4,12 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
-
 import 'package:authentication_repo/authentication_repo.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:podium/bootstrap.dart';
+import 'package:podium/firebase_options.dart';
 import 'package:podium/src/app/app.dart';
 
 Future<void> main() async {
@@ -17,7 +17,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = AppBlocObserver();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final authenticationRepository = AuthenticationRepository();
   await authenticationRepository.user.first;
