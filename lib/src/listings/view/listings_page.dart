b029ea1d748948f4b024c2c1df5efcbc7866bcf4
Darren Via <darren.via@array.com>
2023-03-24 00:38:08 -0700

    images work

--- lib/src/listings/view/listings_page.dart
+++ lib/src/listings/view/listings_page.dart
@@ -5,8 +5,9 @@ class ListingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: CircularProgressIndicator(),
+    return SizedBox(
+      child: Image.network(
+          'https://firebasestorage.googleapis.com/v0/b/podium-78b4e.appspot.com/o/coming-soon-image.png?alt=media&token=e6607041-9bb7-4b9b-9255-bbefd5faf86e'),
     );
   }
 }
