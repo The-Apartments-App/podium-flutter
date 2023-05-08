b796f7b62434fa62a331e42fa67c5e894e21b57e
Kelly <kellyshao728@gmail.com>
2023-05-07 22:02:29 -0700

    Resident Documents Page: Max-Width set to 900 + Aligned to Top Center

--- lib/src/resident_portal/user_documents/view/user_documents_page.dart
+++ lib/src/resident_portal/user_documents/view/user_documents_page.dart
@@ -16,119 +16,136 @@ class DocumentsPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16, 40, 0, 40),
-            child: Text(
-              'Documents',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              showDialog<void>(
-                context: context,
-                builder: (context) {
-                  return Dialog(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final maxWidth = (MediaQuery.of(context).size.width > 900
+                    ? 900
+                    : MediaQuery.of(context).size.width)
+                .toDouble();
+            return ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: maxWidth),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(16, 40, 0, 40),
+                      child: Text(
+                        'Documents',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
                     ),
-                    elevation: 16,
-                    child: const LeaseReader(),
-                  );
-                },
-              );
-            },
-            child: const Padding(
-              padding: EdgeInsets.all(12),
-              child: Text(
-                'View Lease',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
+                    TextButton(
+                      onPressed: () {
+                        showDialog<void>(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                              elevation: 16,
+                              child: const LeaseReader(),
+                            );
+                          },
+                        );
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          'View Lease',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      indent: 25,
+                      endIndent: 25,
+                      thickness: 0,
+                      color: Colors.grey.shade400,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        showDialog<void>(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                              elevation: 16,
+                              child: const LeaseReader(),
+                            );
+                          },
+                        );
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          'Terms of service',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      indent: 25,
+                      endIndent: 25,
+                      thickness: 0,
+                      color: Colors.grey.shade400,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        showDialog<void>(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                              elevation: 16,
+                              child: const LeaseReader(),
+                            );
+                          },
+                        );
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          'Privacy policy',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      indent: 25,
+                      endIndent: 25,
+                      thickness: 0,
+                      color: Colors.grey.shade400,
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ),
-          Divider(
-            indent: 25,
-            endIndent: 25,
-            thickness: 0,
-            color: Colors.grey.shade400,
-          ),
-          TextButton(
-            onPressed: () {
-              showDialog<void>(
-                context: context,
-                builder: (context) {
-                  return Dialog(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                    elevation: 16,
-                    child: const LeaseReader(),
-                  );
-                },
-              );
-            },
-            child: const Padding(
-              padding: EdgeInsets.all(12),
-              child: Text(
-                'Terms of service',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ),
-          Divider(
-            indent: 25,
-            endIndent: 25,
-            thickness: 0,
-            color: Colors.grey.shade400,
-          ),
-          TextButton(
-            onPressed: () {
-              showDialog<void>(
-                context: context,
-                builder: (context) {
-                  return Dialog(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                    elevation: 16,
-                    child: const LeaseReader(),
-                  );
-                },
-              );
-            },
-            child: const Padding(
-              padding: EdgeInsets.all(12),
-              child: Text(
-                'Privacy policy',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ),
-          Divider(
-            indent: 25,
-            endIndent: 25,
-            thickness: 0,
-            color: Colors.grey.shade400,
-          ),
-        ],
+            );
+          },
+        ),
       ),
     );
   }
