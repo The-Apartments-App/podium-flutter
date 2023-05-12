9370dca1e18ee4c5314f4b3d4b7f6f8c85af9535
Darren Via <aivnerrad1993@gmail.com>
2023-05-11 19:51:15 -0700

    update

--- test/widget/linkedin_link/linkedin_link_test.dart
+++ test/widget/linkedin_link/linkedin_link_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:podium/shared/shared_components.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   testWidgets('LinkedInLink renders and redirects correctly',
