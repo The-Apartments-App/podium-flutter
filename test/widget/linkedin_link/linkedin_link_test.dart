159710c8d064e9c451e973e7462d854a579ae88c
Darren Via <aivnerrad1993@gmail.com>
2023-05-11 19:45:32 -0700

    update

--- test/widget/linkedin_link/linkedin_link_test.dart
+++ test/widget/linkedin_link/linkedin_link_test.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:podium/shared/shared_components.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  testWidgets(
-    'LinkedInLink renders correctly',
-    (WidgetTester tester) async {
-      // Build the widget
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: Scaffold(
-            body: LinkedInLink(),
-          ),
+  testWidgets('LinkedInLink renders and redirects correctly',
+      (WidgetTester tester) async {
+    // Build the widget
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: LinkedInLink(),
         ),
-      );
+      ),
+    );
 
-      // Verify that the LinkedIn icon is displayed
-      expect(find.byType(FaIcon), findsOneWidget);
-    },
-  );
+    // Verify that the LinkedIn icon is displayed
+    expect(find.byType(FaIcon), findsOneWidget);
+
+    // Tap on the LinkedIn icon
+    await tester.tap(find.byType(FaIcon));
+    await tester.pumpAndSettle();
+  });
 }
