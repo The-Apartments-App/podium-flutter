9baa435b542ca4d912b7afcbcacb7b3ebec5f1a1
Darren Via <aivnerrad1993@gmail.com>
2023-06-01 19:12:30 -0700

    update

--- lib/src/splash_page/views/splash_page.dart
+++ lib/src/splash_page/views/splash_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: dead_code
+
 import 'package:authentication_repo/authentication_repo.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
