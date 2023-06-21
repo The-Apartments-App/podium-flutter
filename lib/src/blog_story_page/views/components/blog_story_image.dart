90bbfa4ff871c5c1cef7e31ffe145dd13763c267
Darren Via <aivnerrad1993@gmail.com>
2023-06-21 07:56:56 -0700

    update

--- lib/src/blog_story_page/views/components/blog_story_image.dart
+++ lib/src/blog_story_page/views/components/blog_story_image.dart
@@ -10,43 +10,51 @@ class BlogStoryImage extends StatelessWidget {
 
   final String blogImageName;
 
+  Future<String?> getBlogImage(String assetName) async {
+    final storage = FirebaseStorage.instance;
+    final ref =
+        storage.ref().child('gs://podium-78b4e.appspot.com/$assetName.jpg');
+    return ref.getDownloadURL();
+  }
+
   @override
   Widget build(BuildContext context) {
-    String? url;
-
-    Future<String> getBlogImage(String assetName) async {
-      final storage = FirebaseStorage.instance;
-      final ref =
-          storage.ref().child('gs://podium-78b4e.appspot.com/$assetName.jpg');
-      return url = await ref.getDownloadURL();
-    }
-
-    getBlogImage(blogImageName);
-    debugPrint('url: $url');
-
-    return FractionallySizedBox(
-      widthFactor: 1.3,
-      child: ClipRRect(
-        borderRadius: const BorderRadius.all(Radius.circular(16)),
-        child: ProgressiveImage(
-          placeholder: Image.asset(
-            'lib/src/assets/images/podium_logo_round.png',
-            fit: BoxFit.cover,
-          ).image,
-          thumbnail: Image.asset(
-            'lib/src/assets/images/$blogImageName.jpg',
-            fit: BoxFit.cover,
-          ).image,
-          image: url != null
-              ? NetworkImage(url!)
-              : Image.asset(
+    return FutureBuilder<String?>(
+      future: getBlogImage(blogImageName),
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          final url = snapshot.data;
+          debugPrint('url: $url');
+          return FractionallySizedBox(
+            widthFactor: 1.3,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(16)),
+              child: ProgressiveImage(
+                placeholder: Image.asset(
                   'lib/src/assets/images/podium_logo_round.png',
                   fit: BoxFit.cover,
                 ).image,
-          width: MediaQuery.of(context).size.width,
-          height: 700,
-        ),
-      ),
+                thumbnail: Image.asset(
+                  'lib/src/assets/images/$blogImageName.jpg',
+                  fit: BoxFit.cover,
+                ).image,
+                image: url != null
+                    ? NetworkImage(url)
+                    : Image.asset(
+                        'lib/src/assets/images/podium_logo_round.png',
+                        fit: BoxFit.cover,
+                      ).image,
+                width: MediaQuery.of(context).size.width,
+                height: 700,
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
