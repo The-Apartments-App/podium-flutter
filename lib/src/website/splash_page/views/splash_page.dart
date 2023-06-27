71e283409e88ab9eac3bf27e68a5e0584f1623f7
Darren Via <aivnerrad1993@gmail.com>
2023-06-27 09:48:48 -0700

    update

--- lib/src/website/splash_page/views/splash_page.dart
+++ lib/src/website/splash_page/views/splash_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element
+
 import 'package:authentication_repo/authentication_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
